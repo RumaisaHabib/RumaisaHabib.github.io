51cecbf0c067a3bfac47367e39e646ed1703c968
RumaisaHabib <rumi@DN0a23077f.SUNet>
2025-01-23 16:21:52 -0800

    testing sites

---
+++ testing/simple1.js
@@ -0,0 +1,5 @@
+var a=document
+var b=a.getElementById("para")
+var t=a.createElement("p")
+t.innerHTML="I was fetched remotely."
+b.appendChild(t)
---
+++ testing/simple2.js
@@ -0,0 +1,5 @@
+var a=document
+var b=a.getElementById("para")
+var t=a.createElement("p")
+t.innerHTML="I was also fetched remotely. And I can access temp" + temp
+b.appendChild(t)
