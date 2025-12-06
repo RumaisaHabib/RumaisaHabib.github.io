2a6a22406c373a224449f8ef7d30e350b7ccc413
RumaisaHabib <rumi@Rumaisas-MacBook-Air-2.local>
2025-12-06 12:34:39 -0800

    Resume update

--- testing/simple1.js
+++
@@ -1,5 +0,0 @@
-var a=document
-var b=a.getElementById("para")
-var t=a.createElement("p")
-t.innerHTML="I was fetched remotely."
-b.appendChild(t)
--- testing/simple2.js
+++
@@ -1,5 +0,0 @@
-var a=document
-var b=a.getElementById("para")
-var t=a.createElement("p")
-t.innerHTML="I was also fetched remotely. And I can access temp" + temp
-b.appendChild(t)
